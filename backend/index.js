6de2cd58e9851dd6109d937086c42b379821f765
acheney <andrewkkchen@gmail.com>
2024-08-23 14:33:40 -0700

    initialized express fetch and pull endpoints

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
+import fetchArbOpportunities from "./routes/fetchArbOpportunities.js";
+import pullArbOpportunities from "./routes/pullArbOpportunities.js";
 
 const app = express();
 const PORT = process.env.PORT || 5001;
@@ -9,6 +11,19 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+/**
+ * Fetch arb opportunities route
+ */
+app.use("/api/fetch_arb_data", fetchArbOpportunities);
+
+/**
+ * Pull arb opportunities route
+ */
+app.use("/api/pull_arb_data", pullArbOpportunities);
+
+/**
+ * Default route
+ */
 app.get("/", (req, res) => {});
 
 app.listen(PORT, () => {
---
+++ backend/routes/pullArbOpportunities.js
@@ -0,0 +1,19 @@
+import express from "express";
+import axios from "axios";
+
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+  try {
+    const response = await axios.get(
+      "http://localhost:5000/api/arb_opportunity?get_latest_data=true"
+    );
+    res.status(response.status).json(response.data);
+  } catch (error) {
+    res.status(error.response ? error.response.status : 500).json({
+      error: error.message,
+    });
+  }
+});
+
+export default router;
